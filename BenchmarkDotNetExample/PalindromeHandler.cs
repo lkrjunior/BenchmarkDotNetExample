8c5ccfc09a7cfff1aedf0cd8c1ca019b324c9bd6
Luciano Junior Kruse Rocha <luciano.rocha@linx.com.br>
2022-07-05 09:42:07 -0300

    Benchmark example

---
+++ BenchmarkDotNetExample/PalindromeHandler.cs
@@ -0,0 +1,80 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDotNetExample
+{
+    [MemoryDiagnoser]
+    public class PalindromeHandler
+    {
+        //[Params("osso", "arara", "lucianoaonaicul", "lucianoonaicul", "luciano")]
+        [Params("arara")]
+        public string value { get; set; }
+
+        [Benchmark]
+        public bool PalindromeV1()
+        {
+            var invertWord = "";
+
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                invertWord += value.Substring(i, 1);
+            }
+
+            var isTrue = value == invertWord;
+
+            return (isTrue);
+
+        }
+
+        [Benchmark]
+        public bool PalindromeV2()
+        {
+            var invertWord = "";
+
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                invertWord += value[i];
+            }
+
+            var isTrue = value == invertWord;
+
+            return (isTrue);
+
+        }
+
+        [Benchmark]
+        public bool PalindromeV3()
+        {
+            var mod = value.Length % 2;
+            var half = value.Length / 2;
+
+            var finalWord = "";
+
+            for (int i = value.Length - 1; i >= (half + mod); i--)
+            {
+                finalWord += value[i];
+            }
+
+            var isTrue = value.Substring(0, half) == finalWord;
+
+            return (isTrue);
+        }
+
+        [Benchmark]
+        public bool PalindromeV4()
+        {
+            var mod = value.Length % 2;
+            var half = value.Length / 2;
+
+            var finalWord = "";
+
+            for (int i = value.Length - 1; i >= (half + mod); i--)
+            {
+                finalWord += value.Substring(i, 1);
+            }
+
+            var isTrue = value.Substring(0, half) == finalWord;
+
+            return (isTrue);
+        }
+    }
+}
---
+++ BenchmarkDotNetExample/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using BenchmarkDotNetExample;
+
+Console.WriteLine("Hello, World!");
+
+//Run on Release to results
+BenchmarkRunner.Run<PalindromeHandler>();
